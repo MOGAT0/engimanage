9cd85b476ab51ef53c70626bcc38917d3d1c5737
mogato sam jefferson <saca.mogato.ui@phinmaed.com>
2025-10-15 08:46:06 +0800

    new update

--- engimanage-app/app/profile.jsx
+++ engimanage-app/app/profile.jsx
@@ -353,7 +353,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: -2 },
     shadowRadius: 10,
     elevation: 5,
-    height: 600,
+    height: 1000,
   },
   userName: {
     fontSize: 24,
