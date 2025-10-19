318be0413ee90fef9eb20a4a463a103030c455a3
mogato sam jefferson <mogatosamjefferson@gmail.com>
2025-10-19 21:44:02 +0800

    Update profile.jsx

--- engimanage-app/app/profile.jsx
+++ engimanage-app/app/profile.jsx
@@ -234,7 +234,7 @@ const Profile = () => {
             <View style={{ flex: 1 }}>
               {imageLoading && (
                 <View style={styles.loadingOverlay}>
-                  <ActivityIndicator size="large" color="#4bb64fff" />
+                  <ActivityIndicator size="large" color="#109c15ff" />
                 </View>
               )}
               <Image
